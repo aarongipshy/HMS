91e222e12b09d115e5a7b2aceb30463995602919
LayheangTob <layh282004@gmail.com>
2025-06-15 16:54:35 +0700

    add

--- resources/views/banner/index.blade.php
+++ resources/views/banner/index.blade.php
@@ -30,7 +30,9 @@
                         @foreach($data as $d)
                         <tr>
                             <td>{{$d->id}}</td>
-                            <td><img width="100" src="{{asset(''.$d->banner_src)}}" /></td>
+                            {{-- <td><img width="100" src="{{asset(''.$d->banner_src)}}" /></td> --}}
+                            <td><img width="100" src="{{ asset('storage/' . $d->banner_src) }}" />
+                            </td>
                             <td>{{$d->alt_text}}</td>
                             <td>{{$d->publish_status}}</td>
                             <td>
